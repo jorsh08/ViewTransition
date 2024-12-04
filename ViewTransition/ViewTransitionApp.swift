f815fc3afa7cbcec71964c0fcf6b6402fcb3ee35
Jorge Verdugo <jorgeverdugo3005@gmail.com>
2024-12-03 19:25:03 -0700

    Initial Commit

---
+++ ViewTransition/ViewTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTransitionApp.swift
+//  ViewTransition
+//
+//  Created by Jorge Verdugo on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
